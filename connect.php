f51a66e657312dea83293388945f2d4074b8f8d8
Brookeharris <brookeharris@mail.usf.edu>
2017-11-26 14:22:13 -0500

    Add files via upload

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-$MyUsername = "yourDatabaseUsername";  // enter your username for mysql
-$MyPassword = "yourFatabasePassword";  // enter your password for mysql
-$MyHostname = "localhost";      // this is usually "localhost" unless your database resides on a different server
+$MyUsername = "root2";  // enter your username for mysql
+$MyPassword = "root";  // enter your password for mysql
+$MyHostname = "47.199.233.120";      // this is usually "localhost" unless your database resides on a different server
 
 $dbh = mysql_pconnect($MyHostname , $MyUsername, $MyPassword);
-$selected = mysql_select_db("yourDatabaseName",$dbh); //Enter your database name here 
+$selected = mysql_select_db("Weather",$dbh); //Enter your database name here 
 ?>
